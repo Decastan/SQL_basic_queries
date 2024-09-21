ac7db17695722637a32fc95f454c6effb3645523
Daniel Castaño <125098694+Decastan@users.noreply.github.com>
2024-09-20 22:00:09 -0500

    adding resources

---
+++ Resources/Adjusting a name in a table.sql
@@ -0,0 +1,10 @@
+
+### Adjusting the name of the column in a table
+ALTER TABLE product_suggestions RENAME COLUMN `ï»¿product_suggestion_id` TO `product_suggestion_id`;
+
+
+
+SELECT *
+FROM bakery.product_suggestions;
+
+
